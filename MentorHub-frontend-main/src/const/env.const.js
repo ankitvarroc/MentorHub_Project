3deb48733d301391177812f8801dde0ec7b13407
AnkitSingh <ankitsingh.naam@gmail.com>
2025-07-15 15:22:25 +0530

    Update env.const.js

--- MentorHub-frontend-main/src/const/env.const.js
+++ MentorHub-frontend-main/src/const/env.const.js
@@ -1,4 +1,4 @@
-const BASE_URL = https://mentorhub-project.onrender.com";
+const BASE_URL = "https://mentorhub-project.onrender.com";
 const RAZORPAY_KEY_ID = process.env.REACT_APP_RAZORPAY_KEY_ID;
 
 export { BASE_URL, RAZORPAY_KEY_ID };
